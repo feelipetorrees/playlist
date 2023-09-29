46da3926bfe9f0bdb0fe7e4162b4309acd17530b
Marcos <feliptorres430@gmail.com>
2023-09-29 11:58:50 -0300

    model

--- model/music.js
+++ model/music.js
@@ -1,1 +1,33 @@
 // Campos que vamos cadastrar da musica
+
+const mongoose = require('mongoose');
+
+
+
+// Criamos o esquema do nosso banco de dados (Os campos que serão necessarios ser salvos do DB)
+const musicSchema = new mongoose.Schema({
+  name:{
+      type: String,
+      require: true,
+  },
+  author:{
+      type: String,
+      require: true,
+},
+
+  linkImage:{
+    type: String,
+    require: true,
+},
+
+  linkMusic:{
+    type: String,
+    require: true,
+},
+
+
+});
+
+// Com o esquema do DB criamos vamos agora, criar um model para esse DB
+module.exports = mongoose.model("Music", musicSchema);
+
